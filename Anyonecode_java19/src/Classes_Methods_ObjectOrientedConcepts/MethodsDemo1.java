f5daefa1fdf8a7f4fb69276055bf955c4e18222d
sureshnemani <nemani.sureshbabu@gmail.com>
2019-09-15 18:55:05 +0530

    methods

---
+++ Anyonecode_java19/src/Classes_Methods_ObjectOrientedConcepts/MethodsDemo1.java
@@ -0,0 +1,35 @@
+package Classes_Methods_ObjectOrientedConcepts;
+
+public class MethodsDemo1 {
+	
+	
+	public static void studentGrade(String name, int score) {
+		String grade;
+		if(score >=90 && score<=100)
+		{
+			grade="A";
+		}
+		else if(score>=80 && score<=90)
+		{
+			grade="b";
+		}
+		else if(score>=70 && score<=60)
+		{
+			grade="c";
+		}else
+		{
+			grade="failed";
+		}
+		System.out.println("StudentName :"+name +" Grade is :"+ grade);
+	}
+
+
+	public static void main(String[] args) {
+		
+		
+		studentGrade("jhon",92);
+		studentGrade("Tom",80);
+		
+	}
+
+}
---
+++ Anyonecode_java19/src/Classes_Methods_ObjectOrientedConcepts/UnderstadingReturnType.java
@@ -0,0 +1,66 @@
+package Classes_Methods_ObjectOrientedConcepts;
+
+public class UnderstadingReturnType {
+
+	public static void main(String[] args) {
+		
+		String grade=Sgrade(90);
+		String name="john";
+		displaygrades(name,grade);
+		
+		String grade1=Sgrade(80);
+		String name1="john";
+		displaygrades(name1,grade1);
+		
+		//or
+		
+		String grd;
+		String name2;
+		
+		name2="jesicca";
+		grd=Sgrade(90);
+		
+		displaygrades(name2,grd);
+		
+		name2="beil";
+		grd=Sgrade(90);
+		displaygrades(name2,grd);
+		
+		
+		
+		
+		 
+		
+
+	}
+	
+	public static String Sgrade(int score) {
+		  String grade;
+		  
+		  if(score>=90 && score<=100)
+		  {
+			  grade="A";
+		  }
+		  else if(score>=80 && score<=90)
+		  {
+			  grade="B";
+		  }
+		  else if(score>=70 && score<=80)
+		  {
+			  grade="C";
+		  }else
+		  {
+			  grade="Failed";
+		  }
+		  
+		  return grade;
+	}
+	
+	public static void displaygrades(String name,String grade) {
+		
+		System.out.println("**************************************");
+		System.out.println("Student name :"+name+" grade "+grade);
+		System.out.println("**************************************");
+	}
+
+}
